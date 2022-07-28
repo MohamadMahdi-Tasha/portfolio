f2a7f794b38e558180b199f99b874af4ee9bcd35
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-07-28 12:29:24 +0430

    added mqs of second section

--- assets/base/js/variables.js
+++ assets/base/js/variables.js
@@ -1,7 +1,15 @@
 // Made With Love By Mohamad Mahdi Tasha In Neovim
 const header = document.getElementById('header');
-const swiper = new Swiper('.defualt-swiper', {slidesPerView: 4, mousewheel: true,})
 const secondSectionsSwiperWrapper = document.getElementById('second-section-swiper-wrapper');	
 const recentWorks = document.getElementById('recent-works');
 const skeltonParentOfThirdSection = document.querySelectorAll('.skelton-parent-of-third-section');
 const skeltonOfSecondSection = document.querySelectorAll('.skelton-of-second-section');	
+
+const swiper = new Swiper('.defualt-swiper', {
+	slidesPerView: 4,
+	mousewheel: true,
+	breakpoints: {
+ 	1: {slidesPerView: 1,},
+	991: {slidesPerView: 4,},
+  }
+})
