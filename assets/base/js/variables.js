a246528a9f33ca0b25869dbeff20b37f96d7be4b
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-07-30 14:12:31 +0430

    changed some codes location

--- assets/base/js/variables.js
+++ assets/base/js/variables.js
@@ -4,6 +4,10 @@ const secondSectionsSwiperWrapper = document.getElementById('second-section-swip
 const recentWorks = document.getElementById('recent-works');
 const skeltonParentOfThirdSection = document.querySelectorAll('.skelton-parent-of-third-section');
 const skeltonOfSecondSection = document.querySelectorAll('.skelton-of-second-section');	
+const navToggle = document.getElementById('nav-toggler');
+const nav = document.getElementById('nav');
+const darkBgOfNav = document.getElementById('dark-bg-nav');
+const isNavOpened = nav.getAttribute('data-opened');
 
 const swiper = new Swiper('.defualt-swiper', {
 	slidesPerView: 4,
