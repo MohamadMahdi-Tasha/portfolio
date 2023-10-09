1ae23aa8fc1c4ce72eb383b7498916f42ecc70f0
Mahdi Tasha <imwhdiiii@gmail.com>
2023-10-09 17:31:52 +0330

    added some style to menu main component

--- component/dashboard/dashboardMainMenuComponent.tsx
+++ component/dashboard/dashboardMainMenuComponent.tsx
@@ -26,19 +26,44 @@ export default function DashboardMainMenuComponent():ReactNode {
                     <div className={'mt-[20px] p-[15px] rounded-[10px] bg-theme-orange-dark mb-[20px] overflow-hidden'}>
                         <div className={'flex line-bg bg-cover rounded-[10px] relative h-[39px]'}>
                             <button
-                                className={'bg-sky-500 z-[2] absolute whitespace-nowrap top-0 px-[20px] py-[10px] rounded-[5px] text-sky-700 font-bold text-[14px] tracking-wide shadow-[0_0.5em_0] shadow-sky-800 translate-y-[-10px] transition-all hover:shadow-transparent hover:translate-y-0 duration-400 active:translate-y-0 active:shadow-[0_inset_-0.5em_0]'}
+                                className={'bg-sky-500 z-[2] absolute whitespace-nowrap top-0 px-[20px] py-[10px] rounded-[5px] text-sky-700 font-bold text-[14px] tracking-wide shadow-[0_0.5em_0] shadow-sky-800 translate-y-[-10px] transition-all hover:shadow-transparent hover:translate-y-0 duration-400 active:translate-y-0'}
                             >
                                 CONTACT ME
                             </button>
                             <span className={'block absolute left-[70%] top-[50%] translate-y-[-50%]'}><IconComponent name={'right-arrow-stroke'} size={30} /></span>
                         </div>
                     </div>
-                    <div className={'p-[10px] bg-theme-orange-dark rounded-[10px]'}>
-                        <p className={'text-white font-light line-clamp-5 text-[14]'}>
+                    <div className={'p-[10px] bg-theme-orange-dark rounded-[10px] h-[300px] overflow-auto'}>
+                        <p className={'text-white font-light text-[15px]'}>
+                            <span className={'text-[17px] font-bold block truncate mb-[5px]'}>About Me:</span>
                             I'm <span className={'font-bold'}>mahdi tasha</span> <br/>
                             A creative <span className={'font-bold'}>front end developer</span> <br/>
                             Based in <span className={'font-bold'}>tehran</span> <br/>
                             With <span className={'font-bold'}>infinite</span> love for this industry :)
+                        </p>
+                        <hr className={'border-white/30 my-[20px]'} />
+                        <p className={'text-white font-light text-[15px]'}>
+                            <span className={'text-[17px] font-bold block truncate mb-[5px]'}>Skills:</span>
+                            HTML 5 <br/>
+                            CSS 3 <br/>
+                            Javascript + ES6 <br/>
+                            GIT <br/>
+                            REACT JS <br/>
+                            VITE <br/>
+                            REACT NATIVE <br/>
+                            NEXT JS <br/>
+                            TAILWINDCSS <br/>
+                            BOOTSTRAP <br/>
+                            PHOTOPSHOP <br/>
+                            TYPESCRIPT <br/>
+                            REDUX
+                        </p>
+                        <hr className={'border-white/30 my-[20px]'} />
+                        <p className={'text-white font-light text-[15px]'}>
+                            <span className={'text-[17px] font-bold block truncate mb-[5px]'}>My Reason To Became Front End dev:</span>
+                            Before i even learn about this, I knew some Photoshop skills <br/>
+                            And i really liked it. After that which i came up with coding ideas <br/>
+                            I realized i should do front end developer stuff because i like colors and design
                         </p>
                     </div>
                 </div>
